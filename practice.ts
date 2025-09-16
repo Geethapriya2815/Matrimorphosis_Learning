1055717428b08471f8a3c6b7c5feb74791d3f256
Geethapriya2815 <s.l.geethapriya8888@gmail.com>
2025-09-16 16:24:58 +0530

    update my file

--- practice.ts
+++ practice.ts
@@ -20,3 +20,49 @@ function displayUserName(msg: string, names: string = "guest") {
 
 }
 displayUserName("Welcome","geetha")
+
+
+function displayFullName(firstname: string, ...othernames: string[]) { 
+    console.log(firstname+othernames.join(" "))
+}
+
+displayFullName("Geetha", "priya", "sundaraj")
+
+function calculateTotalPrice(...charges: number[]) {
+    let total = 0;
+    for (let i of charges) {
+        total += i;
+    }
+    return total;
+}
+
+
+console.log(calculateTotalPrice(20, 30, 40));      
+console.log(calculateTotalPrice(30, 90, 80, 90));  
+interface Product{
+    productName: string,
+    productPrice: number
+}
+
+let pd1: Product = { productName: "Mobile", productPrice: 20000 }
+let pd2: Product = { productName: "window", productPrice: 300 }
+let pd3: Product = { productName:"Biscuits", productPrice: 3 }
+
+function addProductPrice(...pdt: Product[]) {
+  let total = 0;
+  pdt.map((p) => (total += p.productPrice));
+  return total;
+}
+console.log(addProductPrice(pd1, pd2));
+
+function calculateArea(radius: number): number; //circle
+function calculateArea(l: number, b: number): number; //rectangle
+function calculateArea(a: number, b?: number): number {
+  const PI = 3.14;
+  if (b) {
+    //formula for area of rectangle
+    return a * b;
+  } else return PI * a * a;
+}
+
+console.log(calculateArea(2, 2));
