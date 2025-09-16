25c788cf760aadb80a889670924f4a31ac1d52ce
Geethapriya2815 <s.l.geethapriya8888@gmail.com>
2025-09-16 15:51:04 +0530

    update my file

--- practice.ts
+++ practice.ts
@@ -5,3 +5,18 @@ let newnum: number = Number(data) - 5
 console.log(newnum)
 console.log(age)
 console.log(typeof data)
+
+function displayUser(fname: string, lastname?: string) {
+    if (lastname) console.log("User name is", fname + lastname);
+    else console.log("User name is", fname);
+
+}
+displayUser("Geethapriya");
+displayUser("Geetha", "priya")
+
+function displayUserName(msg: string, names: string = "guest") {
+    console.log(msg,names)
+
+
+}
+displayUserName("Welcome","geetha")
