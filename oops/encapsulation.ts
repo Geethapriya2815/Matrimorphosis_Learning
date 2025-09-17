647914d11d12b0e24a0e6eff8d94c44455e15ee1
Geethapriya2815 <s.l.geethapriya8888@gmail.com>
2025-09-17 16:44:25 +0530

    update all my changes

--- oops/encapsulation.ts
+++ oops/encapsulation.ts
@@ -1,15 +1,11 @@
-class Wallet{
-    private balance;
-    constructor(initialBalance: number) {
-        this.balance = initialBalance;
-    addMoneyToWallet(amount: number) { 
-            if (amount > 0) this.balance += amount;
-        }
-        
-    }
-let userobj = new Wallet(2000)
-console.log("new wallet balance", userobj.addMoneyToWallet(5000))
-
-
-function addMoneyToWallet(amount: number) {
+class Wallet {
+  private balance;
+  constructor(initialBalance: number) {
+    this.balance = initialBalance;
+  }
+  addMoneyToWallet(amount: number) {
+    if (amount > 0) return (this.balance += amount);
+  }
 }
+let userobj = new Wallet(2000);
+console.log("new wallet balance", userobj.addMoneyToWallet(1000));
